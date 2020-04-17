4bbb576a2604d4aa41420551abc48025c51e0cf4
GeorgyBelski <scorp1688@gmail.com>
2020-04-17 14:17:38 +0200

    Add  player movement controller

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals
+{
+    public static int groundLayer = 8;
+    public static int groundMask = 1 << groundLayer;
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float speed = 10;
+
+    new Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        Moving();
+        Turning();
+    }
+    void Moving()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 inputAxisVector =  new Vector3(x, 0, z).normalized;
+        transform.position += inputAxisVector * speed * Time.deltaTime;
+
+    }
+    void Turning()
+    {
+        Ray camRay = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(camRay, out RaycastHit floorHit, 30, Globals.groundMask))
+        {
+            transform.LookAt(new Vector3(floorHit.point.x, transform.position.y, floorHit.point.z));
+        }
+    }
+}
