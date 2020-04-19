20849bd8e4a12ced0482af70461be0ffce6fd89f
GeorgyBelski <scorp1688@gmail.com>
2020-04-19 12:15:15 +0200

    Add enemy movement

--- Assets/Scripts/Enemy/EnemyMovementController.cs
+++ Assets/Scripts/Enemy/EnemyMovementController.cs
@@ -6,10 +6,13 @@ public class EnemyMovementController : MonoBehaviour
 {
     public float speed = 9;
     public float reactionTime = 0.2f;
-    float timerReactionTime; 
+    public Transform enemy;
+    float timerReactionTime;
+    Transform player;
     void Start()
     {
-        
+        if (!enemy) { enemy = transform.Find("Enemy"); }
+        player = PlayerHealth.player.transform;
     }
 
     void FixedUpdate()
@@ -30,8 +33,14 @@ public class EnemyMovementController : MonoBehaviour
         if(timerReactionTime <= 0)
         {
             timerReactionTime = reactionTime;
-            transform.LookAt(PlayerHealth.player.transform.position);
+            enemy.transform.LookAt(player.position);
         }
-        transform.position += transform.rotation * Vector3.forward * speed * Time.deltaTime;
+        float distanceToPlayer = (transform.position - player.position).magnitude;
+
+        if (distanceToPlayer > 2.1f)
+        { 
+
+            transform.position += enemy.transform.rotation * Vector3.forward * speed * Time.deltaTime; 
+        }
     }
 }
